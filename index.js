54ff89371d5b48114224515385c4d274e2895254
kaf212 <kaf221122@gmail.com>
2023-11-13 19:04:51 +0100

    nav verbessert

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 function loadNavElements() {
-    const nav = "<nav id='nav' style='position: fixed'> <div id='navContent' class='container d-flex ' style='height: 100%'> <div class='col-4 d-flex align-items-center' style='height: 100%'> <span id='navTitle'><em><strong>Wirtschaftsgeschichte 2</strong></em></span> </div> <div class='flex-spacer'></div> <div class='nav-option'> <span>Home</span> </div> <div class='nav-option'> <span>Kontakt</span> </div> <div class='nav-option'> <span>Über uns</span> </div> </div> </nav>"
+    const nav = "<nav id='nav' style='position: fixed'> <div id='navContent' class='container d-flex ' style='height: 100%'> <div class='col-4 d-flex align-items-center' style='height: 100%'> <span id='navTitle'><em><strong>Wirtschaftsgeschichte 2</strong></em></span> </div> <div class='flex-spacer'></div> <a href='./index.html' class='nav-option'><div> <span>Home</span> </div></a> <a href='./content.html' class='nav-option'><div class='nav-option'> <span>Wiki</span> </div></a> <a href='' class='nav-option'><div class='nav-option'> <span>Über uns</span> </div></a> </div> </nav>"
 
-    Array.from(document.getElementsByClassName("nav-header")).forEach((h)=> {
+    Array.from(document.getElementsByClassName("nav-header")).forEach((h) => {
         const temp = h.innerHTML
         h.innerHTML = nav
         h.innerHTML += temp
