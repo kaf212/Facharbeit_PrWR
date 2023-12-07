1230ebcf4bf6694ae3e2d454652a070762f90f2a
kaf212 <kaf221122@gmail.com>
2023-12-07 16:29:52 +0100

    made form functional

--- contact.js
+++ contact.js
@@ -12,3 +12,31 @@ document.getElementById("formAnonymity").addEventListener("click", (event)=>{
     }
 
 })
+
+
+document.getElementById("contactForm").addEventListener("submit", (event)=> {
+    event.preventDefault()
+    const form = event.currentTarget
+    const data = new FormData(form)
+
+    const targetMail = "jan.atzgerstorfer@student.ksh.ch"
+    const mailSubject = `${data.get("subject")} bezüglich der Facharbeit PrWR auf GitHub.io`
+    let body = ""
+    console.log(data.get("anonymous"))
+    if (data.get("anonymous") !== "on") { // Wenn der User nicht anonym bleiben will
+        console.log("not anonymous")
+        body += `Anonym:\tNein\n`
+        body += `Name:\t\t${data.get("firstName")} ${data.get("lastName")}\n`
+        body += `E-mail:\t\t${data.get("email")}\n`
+    }
+
+    else {
+        body += `Anonym:\tJa`
+
+    }
+    body += `\nNachricht:\n${data.get("message")}\n\n`
+    window.location.href = 'mailto:' + targetMail + '?subject=' + encodeURIComponent(mailSubject) + '&body=' + encodeURIComponent(body)
+
+
+
+})
